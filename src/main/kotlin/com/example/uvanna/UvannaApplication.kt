e366c48f7b0d125f7b09ea0fa04b8db58bfebb22
Ахунов Денис <60143913+DeNyWho@users.noreply.github.com>
2022-10-02 01:08:44 +0500

    Initial commit

---
+++ src/main/kotlin/com/example/uvanna/UvannaApplication.kt
@@ -0,0 +1,11 @@
+package com.example.uvanna
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UvannaApplication
+
+fun main(args: Array<String>) {
+    runApplication<UvannaApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/uvanna/UvannaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.uvanna
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UvannaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
